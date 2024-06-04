5808092795b7f34bf86da773e658556d67ce9939
Potowai <alexis.thomas.ines@gmail.com>
2024-06-04 16:43:08 +0200

    addd fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        const response = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC', {
+          method:'GET',
+          headers: {
+            "X-CMC_PRO_API_KEY":'533f68e6-ea99-4243-8aca-e68e6eaa2386',
+            'Accept':'application/json',
+            "Access-Control-Allow-Origin":"*"
+          },
+        });
+        if (!response.ok){
+          throw new Error(`HTTP error: Status ${response.status}`);
+        }
+        let price = response.json.data.BTC
+        alert(price);
+        setData(response.json);
+        setError(null);
+      } catch (err){
+        setData(null);
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex">
+      <div className="w-52 sm:w-80 flex justify-center items-center">
+        {loading && (
+          <div className="text-xl font-medium">Loading</div>
+        )}
+        {error && <div className="text-red-700">{error}</div>}
+
+        <div className="bg-gray-100 flex-1 p-4 min-h-[550px]">
+          <p>Hello</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  </div>
+  </>
   )
 }
 
